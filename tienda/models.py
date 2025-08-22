40f2981e6973029b58fc406b95d8891ffa0ada60
Maxiiii22 <maxi.acu222@gmail.com>
2025-08-22 15:42:05 -0300

    Update models.py

--- tienda/models.py
+++ tienda/models.py
@@ -52,20 +52,20 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.PositiveIntegerField()
-    stock = models.PositiveIntegerField() # Solo números enteros positivos
+    stock = models.PositiveIntegerField() 
     fecha_lanzamiento = models.DateField(null=True, blank=True)
     garantia = models.CharField(max_length=50, null=True, blank=True)
     color = models.CharField(max_length=50, null=True, blank=True)
     disponible = models.BooleanField(default=True)  
-    modelo = models.ForeignKey(Modelo,on_delete=models.SET_NULL, null=True, blank=True)  # Relacionamos con el Modelo "Empresa".
-    slug = models.SlugField(default="", null=False, unique=True,)  # Campo de tipo SLUG en donde le valor por defecto es "" , no podra ser NULL y no se debe repetir.
+    modelo = models.ForeignKey(Modelo,on_delete=models.SET_NULL, null=True, blank=True)  
+    slug = models.SlugField(default="", null=False, unique=True,)  
     
     class Meta:
-        abstract = True  # El abstract = True significa que Django no va a crear una tabla para este modelo, pero los hijos sí van a heredar sus campos.
+        abstract = True 
     
     @property  # @property convierte el método modelo_name en un atributo calculado, es decir, puedes acceder a él como si fuera un atributo normal del objeto. No necesitas llamar explícitamente al método desde tu vista; simplemente accedes a producto.modelo_name como si fuera un campo de modelo.
     def modelo_name(self):
-        return self._meta.model_name           # Devuelve el nombre del modelo heredado en minusculas (como "consolas", "relojes", etc.) 
+        return self._meta.model_name  # Devuelve el nombre del modelo heredado en minusculas (como "consolas", "relojes", etc.) 
     
     def __str__(self):
         return self.nombre
@@ -78,7 +78,7 @@ class Producto(models.Model):
 
 
 
-class Juegos(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabla "Producto" y de "ProductoConImagen".
+class Juegos(ProductoConImagen,Producto):  
     CLASIFICACIONES_EDAD = [
         ('E', 'Everyone (E)'),
         ('E10', 'Everyone 10+ (E10+)'),
@@ -90,8 +90,8 @@ class Juegos(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabl
         ('FISICO', 'Físico')
     ]
     
-    generos = models.ManyToManyField("Genero")    # Relacion de Muchos a Muchos con el modelo "Géneros (Crea una tercera tabla JuegosXGenero)"
-    plataforma = models.ForeignKey(Plataforma, on_delete=models.SET_NULL, null=True, blank=True) # Relacionamos con el Modelo "Plataformas" , donde este campo puede estar vacio o ser NULL .
+    generos = models.ManyToManyField("Genero")   
+    plataforma = models.ForeignKey(Plataforma, on_delete=models.SET_NULL, null=True, blank=True)
     edicion = models.CharField(max_length=50,  default="Standar")
     tamaño_juego = models.CharField(max_length=30)
     clasificacion_edad = models.CharField(max_length=4, choices=CLASIFICACIONES_EDAD)
@@ -112,18 +112,16 @@ class Juegos(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabl
             
 
 class ImagenSecundaria(models.Model):  # Tabla donde estaran las imagnes secundarias de los productos
-    producto = models.ForeignKey(ProductoConImagen, on_delete=models.CASCADE, 
-                                related_name="imagenes_secundarias") # El related_name='imagenes_secundarias' en el ForeignKey nos permite crear un nombre para poder acceder a todas las imágenes_secundarias que esten asociadas a una imagen_principa del modelo "ProductoConImagen".
-                                                                    # Solo debemos hacer un for de esta manera por ejemplo : {% for img in consola.imagenes_secundarias.all %} 
-                                                                    # Si no usamos "related_name" deberiamos hacerlo asi : {% for img in consola.ImagenSecundaria_set.all %}
+    producto = models.ForeignKey(ProductoConImagen, on_delete=models.CASCADE, related_name="imagenes_secundarias") 
+                                                                  
     imagen = models.ImageField(upload_to='productos/imagenes_secundarias/')
 
     def __str__(self):
         nombre = str(self.producto)
-        nombre_sin_ext = nombre.rsplit('.', 1)[0]  # Y con esto quitamos el tipo de archivo que es la imagen (.png.jpg,etc)
+        nombre_sin_ext = nombre.rsplit('.', 1)[0]  
         return f"Imagen de {nombre_sin_ext}: {self.imagen.name.split('/')[-1]}"
 
-class Consolas(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabla "Producto" y de "ProductoConImagen".
+class Consolas(ProductoConImagen,Producto):  
     edicion = models.CharField(max_length=50, blank=True, null=True)
     almacenamiento = models.CharField(max_length=50, blank=True, null=True)  
     cant_controles = models.CharField(max_length=50)  
@@ -147,12 +145,12 @@ class Consolas(ProductoConImagen,Producto):  # Hereda los campos de el modelo/ta
             return f"{self.nombre} - Stock: {self.stock}" 
     
 
-class Celulares(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabla "Producto" y de "ProductoConImagen".
+class Celulares(ProductoConImagen,Producto):  
     sistema_operativo = models.CharField(max_length=50)
     almacenamiento = models.CharField(max_length=50)
     procesador = models.CharField(max_length=50)
     ram = models.CharField(max_length=50)
-    pantalla = models.CharField(max_length=100)  # Tamaño y tipo
+    pantalla = models.CharField(max_length=100)  
     resolucion_cam_frontal = models.CharField(max_length=100)  
     resolucion_cam_trasera = models.CharField(max_length=100)  
     
@@ -168,12 +166,12 @@ class Celulares(ProductoConImagen,Producto):  # Hereda los campos de el modelo/t
         super().save(*args, **kwargs)
 
 
-class RelojInteligentes(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabla "Producto" y de "ProductoConImagen".
+class RelojInteligentes(ProductoConImagen,Producto):  
     tipo_pantalla = models.CharField(max_length=50)
     tamaño_pantalla = models.CharField(max_length=50)
     duracion_bateria = models.CharField(max_length=100)
-    conectividad = models.CharField(max_length=100)  # Ej: Bluetooth, Wi-Fi, LTE
-    compatibilidad = models.CharField(max_length=100)  # Ej: Android, iOS
+    conectividad = models.CharField(max_length=100) 
+    compatibilidad = models.CharField(max_length=100) 
     sensores = models.TextField()
     
     def __str__(self):
@@ -196,7 +194,7 @@ class Conexion(models.Model):
     def __str__(self):
         return self.tipo
 
-class AccesoriosPC(ProductoConImagen,Producto):  # Hereda los campos de el modelo/tabla "Producto" y de "ProductoConImagen".
+class AccesoriosPC(ProductoConImagen,Producto): 
     TIPO_ACCESORIO_CHOICES = [
         ('teclado', 'Teclado'),
         ('mouse', 'Mouse'),
@@ -206,7 +204,7 @@ class AccesoriosPC(ProductoConImagen,Producto):  # Hereda los campos de el model
         ('camara', 'Cámara'),
     ]    
     tipo = models.CharField(max_length=30, choices=TIPO_ACCESORIO_CHOICES)
-    conexiones = models.ManyToManyField(Conexion)  # Crea una tabla "accesoriosPcXConexion"
+    conexiones = models.ManyToManyField(Conexion)  
     rgb = models.BooleanField(default=False)
     compatibilidad = models.CharField(max_length=100)  
     microfono_integrado = models.BooleanField(blank=True, null=True)
@@ -214,8 +212,8 @@ class AccesoriosPC(ProductoConImagen,Producto):  # Hereda los campos de el model
     bateria = models.CharField(max_length=50, blank=True, null=True)
     tiempo_bateria = models.CharField(max_length=50, blank=True, null=True)  
     peso = models.CharField(max_length=50)
-    dimensiones = models.CharField(max_length=100, blank=True, null=True)  # Ej: 30x10x5 cm
-    resolucion = models.CharField(max_length=100, blank=True, null=True)  # Resolución de cámara o sensor óptico (ej: "1080p", "8000 DPI").
+    dimensiones = models.CharField(max_length=100, blank=True, null=True) 
+    resolucion = models.CharField(max_length=100, blank=True, null=True) 
 
     def __str__(self):
         if self.stock == 0:
